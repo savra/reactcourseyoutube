aaddc012884ef1892a45305e62b0da59f940c6d9
Sergey Savrukhin <savrawar@mail.ru>
2021-02-05 14:31:48 +0300

    first several tasks

--- src/App.js
+++ src/App.js
@@ -3,34 +3,47 @@ import React from "react";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className="app-wrapper">
+            <header className="header">
+                <img src="logo192.png"/>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a href="">Profile</a>
+                </div>
+                <div>
+                    <a href="">Message</a>
+                </div>
+                <div>
+                    <a href="">News</a>
+                </div>
+                <div>
+                    <a href="">Music</a>
+                </div>
+                <div>
+                    <a href="">Settings</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img src="\images\backgroundImageContent.jpg"/></div>
+                <div>
+                    ava + desc
+                </div>
+                <div>
+                    My post
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        Post1
+                    </div>
+                    <div>
+                        Post2
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
-
-const Technologies = () => {
-    return (
-        <div>
-            <ul>
-                <li>css</li>
-                <li>html</li>
-                <li>JS</li>
-                <li>React</li>
-            </ul>
-        </div>
-    );
-}
-
-const Header = () => {
-    return (
-        <div>
-            <a href="">Home</a>
-            <a href="">News Feed</a>
-            <a href="">Messages</a>
-        </div>
-    );
-}
-
 export default App;
