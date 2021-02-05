e1bc6e757b46838246e9c7315315c7c1b48c3469
Sergey Savrukhin <savrawar@mail.ru>
2021-02-05 11:49:24 +0300

    create some components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href="">Home</a>
+            <a href="">News Feed</a>
+            <a href="">Messages</a>
+        </div>
+    );
 }
 
 export default App;
